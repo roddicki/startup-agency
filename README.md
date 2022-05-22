bdbd83c47487caa03e091a5df1666123fd2a35a9
rod <rod@roddickinson.net>
2022-05-22 18:07:01 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Repository for the Startup Freelancer Agency
+
+Repository for the Startup Freelancer Agency for University of South Wales
+
+
+## This is an npm webpack environment  
+To run / build the environment
+```console
+$ npm run build
+
+```  
+
+To deploy to firebase hosting / build the environment  
+```console  
+$ firebase deploy --only hosting  
+
+```  
+
+## Tutorials
+Setup the local devlopment environment:  
+Follow video 1 & 2  
+https://youtu.be/9zdvmgGsww0  
---
+++ src/index.js
@@ -0,0 +1,16 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAq7-QGjZ8O1RVe_seOfdYjVLCjLdwrHYE",
+  authDomain: "studio-freelancer-agency.firebaseapp.com",
+  projectId: "studio-freelancer-agency",
+  storageBucket: "studio-freelancer-agency.appspot.com",
+  messagingSenderId: "487647550435",
+  appId: "1:487647550435:web:474b8fb15c9412fb005ed6",
+  measurementId: "G-TXX5XFWWN6"
+};
+
+initializeApp(firebaseConfig)
+
+console.log('hello from index.js');
