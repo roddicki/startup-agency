d9469d34496fe1effdf3e40b1e3aff57b8e49d9c
rod <rod@roddickinson.net>
2022-07-09 11:03:53 +0100

    added new entry points and new JS file

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: ['./src/index.js','./src/another-module.js'],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
